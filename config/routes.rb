08ba5d287fb439e139de478dfc1bb32624eb1e91
jorgbot <jbotelloangarita@hotmail.com>
2017-11-18 01:59:46 -0500

    Actualizar perfil mejoras
    -Se elimina la opcion de actualizar el perfil con obligacion a digitar su contraseña para validar...

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
 
   root 'paginas#inicio'
 
-  devise_for :usuarios
+  devise_for :usuarios,
+             path: '',
+             path_names: {sign_in: 'ingresar', sign_out: 'salir', edit: 'perfil', sign_up: 'registrar'},
+             controllers: {registrations: 'registrations'}
 
 end
