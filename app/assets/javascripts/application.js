c6896aac0ce980b131658a87f821d6cddd3b11b7
dialmedu <diegomesa.1414@gmail.com>
2017-12-10 03:32:57 -0500

    Mejora de UX en Formulario
    Ahora decta cuando los campos de un grupo esten completados y activa el oro-form--control correspondiente

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,3 +16,30 @@
 //= require toastr
 //= require turbolinks
 //= require_tree .
+
+
+function loadDatosBasicos(){
+    for( j = 0; j < $('.oro-form__group').length; j++) {
+        for (i = 0; i < $($('.oro-form__group')[j]).find('input').length; i++) {
+            data = $($('.oro-form__group')[j]).attr('id');
+            console.log(data);
+            $($($('.oro-form__group')[j]).find('input')[i]).attr('data-parent', data);
+        }
+    }
+    for(i = 0; i < $('input').length; i++){
+        $($('input')[i]).on('blur',function () {
+            parent = $(this).attr('data-parent');
+            completado = true;
+            for( j=0; j < $('input[data-parent="'+parent+'"]').length; j++ )
+            {
+                if($($('input[data-parent="'+parent+'"]')[j]).val() == ''){
+                    completado = false;
+                }
+            }
+            if(completado){
+                $('a[href="#'+parent+'"]').parent().addClass('oro-form__check--active')
+            }
+        });
+    }
+
+}
