94a88a894cfe01446e5b16938e04c90c137a2613
jorgbot <jbotelloangarita@hotmail.com>
2018-06-06 12:11:40 -0500

    Add action_mailer IP

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,7 +84,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: '206.81.9.1' }
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
